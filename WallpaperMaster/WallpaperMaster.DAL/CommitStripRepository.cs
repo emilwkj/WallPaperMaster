b266c5f654ccbe6b5f83b4768bf8de1b2ea975a5
Emil Wismann Kirkebæk-Jensen <kirkebak@danfoss.com>
2019-09-16 16:02:05 +0200

    Coder makes the most awesome http-repo, look how here

---
+++ WallpaperMaster/WallpaperMaster.DAL/CommitStripRepository.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallpaperMaster.DAL
+{
+    public class CommitStripRepository
+    {
+        public async Task<JObject> GetSite()
+        {
+            return await GetAsync("http://www.commitstrip.com/en/?");
+        }
+        private async Task<JObject> GetAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            var result = await client.SendAsync(request);
+            string stringResult = await result.Content.ReadAsStringAsync();
+
+            return JObject.Parse(stringResult);
+        }
+    }
+}
--- WallpaperMaster/WallpaperMaster.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WallpaperMaster.Service
-{
-    public class Class1
-    {
-    }
-}
