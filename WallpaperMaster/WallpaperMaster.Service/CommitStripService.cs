09e0f0a099503b48d24d8457e9a2dbeee5765416
Emil Wismann Kirkebæk-Jensen <kirkebak@danfoss.com>
2019-09-16 16:02:46 +0200

    Programmer creates wild super-service

---
+++ WallpaperMaster/WallpaperMaster.Service/CommitStripService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WallpaperMaster.DAL;
+
+namespace WallpaperMaster.Service
+{
+    public class CommitStripService
+    {
+        private readonly CommitStripRepository _commitStripRepository;
+        public CommitStripService(CommitStripRepository commitStripRepository)
+        {
+            _commitStripRepository = commitStripRepository;
+        }
+        public void GetPicture()
+        {
+
+        }
+    }
+}
