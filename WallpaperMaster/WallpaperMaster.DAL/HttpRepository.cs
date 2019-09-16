3a5cd5b152fa0405004de9e9652e98e2d4326e7d
Emil Wismann Kirkebæk-Jensen <kirkebak@danfoss.com>
2019-09-16 16:59:57 +0200

    Guy makes the coolest wallpaper-repo to change the windows wallpaper, look how here

---
+++ WallpaperMaster/WallpaperMaster.DAL/HttpRepository.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallpaperMaster.DAL
+{
+    public class HttpRepository
+    {
+        public async Task<string> GetSite(string url)
+        {
+            return await GetAsync(url);
+        }
+        private async Task<string> GetAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            var result = await client.SendAsync(request);
+            string stringResult = await result.Content.ReadAsStringAsync();
+            return stringResult;
+        }
+        public bool DownloadPicture(string url, string downloadLocation)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(new Uri(url), downloadLocation);
+                }
+                return true;
+            }
+            catch {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ WallpaperMaster/WallpaperMaster.DAL/WallPaperRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallpaperMaster.DAL
+{
+    public class WallPaperRepository
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern int SystemParametersInfo(UAction uAction, int uParam, StringBuilder lpvParam, int fuWinIni);
+        public enum UAction
+        {
+            /// <summary>
+            /// set the desktop background image
+            /// </summary>
+            SPI_SETDESKWALLPAPER = 0x0014,
+            /// <summary>
+            /// set the desktop background image
+            /// </summary>
+            SPI_GETDESKWALLPAPER = 0x0073,
+        }
+        public int SetBackGround(string wallpaperFile)
+        {
+            int result = 0;
+            if (File.Exists(wallpaperFile))
+            {
+                StringBuilder s = new StringBuilder(wallpaperFile);
+                result = SystemParametersInfo(UAction.SPI_SETDESKWALLPAPER, 0, s, 0x2);
+            }
+            return result;
+        }
+    }
+}
