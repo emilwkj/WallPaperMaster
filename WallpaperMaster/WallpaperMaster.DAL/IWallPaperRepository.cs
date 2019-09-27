fc858e026dec9231e1f2b0113b4bd1c3dd5f7db0
Emil Wismann Kirkebæk-Jensen <kirkebak@danfoss.com>
2019-09-27 14:02:08 +0200

    Stupid developer forgot to add new files, look how he did here...

---
+++ WallpaperMaster/WallpaperMaster.DAL/IWallPaperRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace WallpaperMaster.DAL
+{
+    public interface IWallPaperRepository
+    {
+        /// <summary>
+        /// Sets the wallpaper
+        /// </summary>
+        /// <param name="tempPath"></param>
+        /// <param name="style">
+        /// Style of wallpaper
+        /// 0 = Tiled
+        /// 1 = Centered
+        /// 2 = Stretched
+        /// </param>
+        void SetWallPaper(string tempPath, int style);
+    }
+}
